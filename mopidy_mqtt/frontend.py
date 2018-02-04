6e4447e8424d465c6198f00d84dff6cefc794150
acolytec3 <kuntow@hotmail.com>
2018-02-04 19:01:12 +0000

    Snips-friendly updates

--- mopidy_mqtt/frontend.py
+++ mopidy_mqtt/frontend.py
@@ -49,60 +49,54 @@ class MQTTFrontend(pykka.ThreadingActor, core.CoreListener):
             logger.warn("Error during subscribe: " + str(rc[0]))
         else:
             logger.info("Subscribed to " + self.topic + "/play")
-        self.mqttClient.subscribe(self.topic + "/control")
-        logger.info("sub:" + self.topic + "/control")
-        self.mqttClient.subscribe(self.topic + "/volume")
-        logger.info("sub:" + self.topic + "/volume")
-
+#       self.mqttClient.subscribe(self.topic + "/control")
+#        logger.info("sub:" + self.topic + "/control")
+#        self.mqttClient.subscribe(self.topic + "/volume")
+#       logger.info("sub:" + self.topic + "/volume")
+#        self.mqttClient.subscribe(self.topic + "/add")
+#        logger.info("sub:" + self.topic + "/add")
     		## Listen for request to refresh all data
         self.mqttClient.subscribe(self.topic + "/current_state")
-
+        self.mqttClient.subscribe("hermes/intent/konjou:playAsong")
+        logger.info("sub: hermes/intent/konjou:playAsong")
+        self.mqttClient.subscribe("hermes/intent/konjou:playAnArtist")
+        self.mqttClient.subscribe("hermes/intent/konjou:nextSong")
+        self.mqttClient.subscribe("hermes/intent/previousSong")
+        self.mqttClient.subscribe("hermes/intent/konjou:speakerInterrupt")
+        self.mqttClient.subscribe("hermes/intent/konjou:resumeMusic")
+        self.mqttClient.subscribe("hermes/intent/konjou:volumeDown")
+        self.mqttClient.subscribe("hermes/intent/volumeUp")
+		
     def mqtt_on_message(self, mqttc, obj, msg):
         logger.info("received a message on " + msg.topic+" with payload "+str(msg.payload))
-        topPlay = self.topic + "/play"
-        topControl = self.topic + "/control"
-        topVolume = self.topic + "/volume"
+
         topCurrentState = self.topic + "/current_state"
 
-        if msg.topic == topPlay:
-            self.core.playback.stop().get()
-            self.core.tracklist.clear().get()
-            logger.info(" Loading in playlist " + msg.payload)
-            self.core.tracklist.add(None, 0, str(msg.payload), None).get()
-            logger.info(self.core.tracklist.get_length().get())
-            logger.info(self.core.tracklist.index().get())
-            self.core.tracklist.shuffle().get() #TODO move this as an option?  play_shuffle?
-            self.core.playback.play()
-        elif msg.topic == topControl:
-            if msg.payload == "stop":
-                self.core.playback.stop()
-            elif msg.payload == "pause":
-                self.core.playback.pause()
-            elif msg.payload == "play":
-                self.core.playback.play()
-            elif msg.payload == "resume":
-                self.core.playback.resume()
-            elif msg.payload == "next":
-                self.core.playback.next()
-                logger.info(self.core.tracklist.get_length().get())
-                logger.info(self.core.tracklist.index().get())
-            elif msg.payload == "previous":
-                self.core.playback.previous()
-            elif msg.payload == "shuffle":
-                self.core.tracklist.shuffle()
-            elif msg.payload == "repeat_on":
-                self.core.tracklist.set_repeat(True)
-            elif msg.payload == "repeat_off":
-                self.core.tracklist.set_repeat(False)
-        elif msg.topic == topVolume:
-            try:
-                volume=int(msg.payload)
-                self.core.mixer.set_volume(volume)
-            except ValueError:
-                logger.warn("invalid payload for volume: " + msg.payload)
+        if msg.topic == "hermes/intent/konjou:speakerInterrupt":
+            self.core.playback.pause()
+        elif msg.topic == "hermes/intent/konjou:resumeMusic":
+            self.core.playback.resume()
+        elif msg.topic == "hermes/intent/konjou:nextSong":
+            self.core.playback.next()
+        elif msg.topic == "hermes/intent/previousSong":
+            self.core.playback.previous()
+        elif msg.topic == "hermes/intent/konjou:volumeDown":
+            volume=10
+            self.core.mixer.set_volume(volume)
+
         elif msg.topic == topCurrentState:
 			      # Publish the current state
             self.send_state()
+        elif msg.topic == "hermes/intent/konjou:playAsong":
+			slots = self.parse_slots(msg)
+			if 'artist' in slots:
+				self.playSong(slots['song'],slots['artist'])
+			else:
+				self.playSong(slots['song'],"")
+        elif msg.topic == "hermes/intent/konjou:playAnArtist":
+			slots = self.parse_slots(msg)
+			if 'artist' in slots:
+				self.playArtist(slots['artist'])
 
     def send_state(self):
         current_track = self.core.playback.get_current_track().get()
@@ -116,7 +110,6 @@ class MQTTFrontend(pykka.ThreadingActor, core.CoreListener):
           track_artist = list(current_track.artists)[0].name
           track_name = current_track.name
           track_uri = current_track.uri
-          # {'tunein:station:s115770': (Image(uri='http://cdn-radiotime-logos.tunein.com/s115770q.png'),)}
           images = []
           imgUris = self.core.library.get_images([track_uri]).get()
           for k in imgUris:
@@ -141,12 +134,27 @@ class MQTTFrontend(pykka.ThreadingActor, core.CoreListener):
     def on_stop(self):
         logger.info("mopidy_mqtt shutting down ... ")
         self.mqttClient.disconnect()
-        
-    #def stream_title_changed(self, title):
-    #    self.send_state()
-        #if self.json_state:  title = {"title": title}
-        #self.MQTTHook.publish("/nowplaying", title)
 
+    def playSong(self, songName, artistName):
+		if artistName == "":
+			result = self.core.library.search({'track_name':[songName]}).get()
+		else:
+			result = self.core.library.search({'track_name':[songName],'artist':[artistName]}).get()
+		track = result[0].tracks   #get list of tracks from search results
+		self.core.tracklist.clear().get()
+		self.core.tracklist.add(None,0,track[0].uri,None).get()
+		self.core.playback.play()
+
+    def playArtist(self, artistname):
+		result = self.core.library.search({'artist':[artistname]}).get()    #search for artist in all backends
+		artist = result[0].tracks								#get list of tracks from artist search
+		self.core.tracklist.clear().get()
+		for track in artist:
+			self.core.tracklist.add(None,0,track.uri,None).get()
+			logger.info(track.name)
+		self.core.tracklist.shuffle().get()
+		self.core.playback.play()
+		
     def playback_state_changed(self, old_state, new_state):
         self.send_state()
         #payload = new_state
@@ -156,23 +164,11 @@ class MQTTFrontend(pykka.ThreadingActor, core.CoreListener):
         #if (new_state == "stopped" and not self.json_state):
         #    self.MQTTHook.publish("/nowplaying", "stopped")
         
-    #def track_playback_started(self, tl_track):
-    #    self.send_state()
-        #track = tl_track.track
-        #artists = ', '.join(sorted([a.name for a in track.artists]))
-        #payload = artists + ":" + track.name
-        #if self.json_state:
-        #    payload = {"title": track.name, "artist": artists}
-        #self.MQTTHook.publish("/nowplaying", payload)
-        #try:
-        #    album = track.album
-        #    payload = next(iter(album.images))
-        #    if self.json_state: 
-        #        payload = {"image_url": albumImage}
-        #    self.MQTTHook.publish("/image", payload)
-        #except:
-        #    logger.debug("no image")
-        
+
+    def parse_slots(self, msg):
+		data = json.loads(msg.payload)
+		return dict((slot['slotName'], slot['value']['value']) for slot in data['slots'])   
+
 class MQTTHook():
     def __init__(self, frontend, core, config, client):
         self.config = config['mqtthook']
